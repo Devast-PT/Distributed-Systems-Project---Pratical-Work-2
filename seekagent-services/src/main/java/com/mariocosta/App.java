de102dd6ec0ebe740f5deb6522f065abbc10a67c
Mario Costa <marioatccosta@gmail.com>
2024-01-14 15:41:46 +0000

    ProjectFolder

---
+++ seekagent-services/src/main/java/com/mariocosta/App.java
@@ -0,0 +1,13 @@
+package com.mariocosta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
